5dd92633b91bc99acab218b9e85067bf33dd878e
Steve Purcell <steve@sanityinc.com>
2011-04-03 16:28:07 +0100

    Add akrito's patch to compensate for broken gamma on recent cocoa emacs

--- color-theme-sanityinc-solarized.el
+++ color-theme-sanityinc-solarized.el
@@ -61,105 +61,125 @@
          (violet  "#6c71c4")
          (blue    "#268bd2")
          (cyan    "#2aa198")
-         (green   "#859900")
-         (foregrounds (list base1 base0 base00 base01))
-         (backgrounds (list base03 base02))
-         (contrast-backgrounds (list base3 base2)))
-    (when (eq 'light mode)
-      (rotatef backgrounds contrast-backgrounds)
-      (setq foregrounds (reverse foregrounds)))
-    (let ((background (first backgrounds))
-          (foreground (second foregrounds))
-          (alt-background (second backgrounds))
-          (contrast-background (first contrast-backgrounds)))
-      (color-theme-install
-       `(,(intern (concat "color-theme-sanityinc-solarized-" (symbol-name'light)))
-         ((background-color . ,background)
-          (background-mode . light)
-          (border-color . ,foreground)
-          (cursor-color . ,magenta)
-          (foreground-color . ,foreground)
-          (mouse-color . ,cyan))
+         (green   "#859900"))
+    (when (and (eq system-type 'darwin) (eq 'ns window-system))
+      (message "tweaking solarized for broken gamma on this emacs")
+      ;; Color patching from akrito on github, compensating for Cocoa Emacs' bad gamma
+      ;; See https://github.com/akrito/solarized/commit/eeef256353be20ac26b5aff6db8b9ab4f91d30d1
+      (setq base03  "#002028"
+            base02  "#122932"
+            base01  "#495a64"
+            base00  "#566770"
+            base0   "#748284"
+            base1   "#849090"
+            base2   "#e9e2cd"
+            base3   "#fbf3e0"
+            yellow  "#a07600"
+            orange  "#ac4223"
+            red     "#bc3332"
+            magenta "#b23873"
+            violet  "#5662b5"
+            blue    "#337ac4"
+            cyan    "#438d84"
+            green   "#798400"))
+    (let ((foregrounds (list base1 base0 base00 base01))
+          (backgrounds (list base03 base02))
+          (contrast-backgrounds (list base3 base2)))
+      (when (eq 'light mode)
+        (rotatef backgrounds contrast-backgrounds)
+        (setq foregrounds (reverse foregrounds)))
+      (let ((background (first backgrounds))
+            (foreground (second foregrounds))
+            (alt-background (second backgrounds))
+            (contrast-background (first contrast-backgrounds)))
+        (color-theme-install
+         `(,(intern (concat "color-theme-sanityinc-solarized-" (symbol-name'light)))
+           ((background-color . ,background)
+            (background-mode . light)
+            (border-color . ,foreground)
+            (cursor-color . ,magenta)
+            (foreground-color . ,foreground)
+            (mouse-color . ,cyan))
 
-         ;; Standard font lock faces
-         (default ((t (nil))))
-         (bold ((t (:bold t))))
-         (bold-italic ((t (:italic t :bold t))))
-         (underline ((t (:underline t))))
-         (italic ((t (:italic t))))
-         (font-lock-builtin-face ((t (:foreground ,green))))
-         (font-lock-comment-delimiter-face ((t (:foreground ,(third foregrounds) :slant italic))))
-         (font-lock-comment-face ((t (:foreground ,(fourth foregrounds) :slant italic))))
-         (font-lock-constant-face ((t (:foreground ,yellow))))
-         (font-lock-doc-face ((t (:foreground ,magenta))))
-         (font-lock-doc-string-face ((t (:foreground ,yellow))))
-         (font-lock-function-name-face ((t (:foreground ,blue))))
-         (font-lock-keyword-face ((t (:foreground ,green :bold t))))
-         (font-lock-preprocessor-face ((t (:foreground ,orange))))
-         (font-lock-string-face ((t (:foreground ,cyan))))
-         (font-lock-type-face ((t (:foreground ,yellow))))
-         (font-lock-variable-name-face ((t (:foreground ,orange))))
-         (font-lock-warning-face ((t (:bold t :foreground ,red))))
+           ;; Standard font lock faces
+           (default ((t (nil))))
+           (bold ((t (:bold t))))
+           (bold-italic ((t (:italic t :bold t))))
+           (underline ((t (:underline t))))
+           (italic ((t (:italic t))))
+           (font-lock-builtin-face ((t (:foreground ,green))))
+           (font-lock-comment-delimiter-face ((t (:foreground ,(third foregrounds) :slant italic))))
+           (font-lock-comment-face ((t (:foreground ,(fourth foregrounds) :slant italic))))
+           (font-lock-constant-face ((t (:foreground ,yellow))))
+           (font-lock-doc-face ((t (:foreground ,magenta))))
+           (font-lock-doc-string-face ((t (:foreground ,yellow))))
+           (font-lock-function-name-face ((t (:foreground ,blue))))
+           (font-lock-keyword-face ((t (:foreground ,green :bold t))))
+           (font-lock-preprocessor-face ((t (:foreground ,orange))))
+           (font-lock-string-face ((t (:foreground ,cyan))))
+           (font-lock-type-face ((t (:foreground ,yellow))))
+           (font-lock-variable-name-face ((t (:foreground ,orange))))
+           (font-lock-warning-face ((t (:bold t :foreground ,red))))
 
-         ;; Flymake
-         (flymake-warnline ((t (:underline ,orange :background ,background))))
-         (flymake-errline ((t (:underline ,red :background ,background))))
+           ;; Flymake
+           (flymake-warnline ((t (:underline ,orange :background ,background))))
+           (flymake-errline ((t (:underline ,red :background ,background))))
 
-         ;; MMM-mode
-         (mmm-code-submode-face ((t (:background ,alt-background))))
-         (mmm-comment-submode-face ((t (:inherit font-lock-comment-face))))
-         (mmm-output-submode-face ((t (:background ,alt-background))))
+           ;; MMM-mode
+           (mmm-code-submode-face ((t (:background ,alt-background))))
+           (mmm-comment-submode-face ((t (:inherit font-lock-comment-face))))
+           (mmm-output-submode-face ((t (:background ,alt-background))))
 
-         ;; Search
-         (isearch ((t (:foreground ,background :background ,green))))
-         (isearch-lazy-highlight-face ((t (:foreground ,foreground :background ,yellow))))
+           ;; Search
+           (isearch ((t (:foreground ,background :background ,green))))
+           (isearch-lazy-highlight-face ((t (:foreground ,foreground :background ,yellow))))
 
-         ;; IDO
-         (ido-subdir ((t (:foreground ,magenta))))
-         (ido-first-match ((t (:foreground ,orange))))
-         (ido-only-match ((t (:foreground ,green))))
+           ;; IDO
+           (ido-subdir ((t (:foreground ,magenta))))
+           (ido-first-match ((t (:foreground ,orange))))
+           (ido-only-match ((t (:foreground ,green))))
 
-         ;; Emacs interface
-         (fringe ((t (:background ,alt-background))))
-         (border ((t (:background ,alt-background))))
-         (border-glyph ((t (nil))))
-         (highlight ((t (:background ,(second contrast-backgrounds)))))
-         (gui-element ((t (:background ,alt-background :foreground ,foreground))))
-         (mode-line ((t (:foreground ,(fourth foregrounds) :background ,alt-background :bold t
-                                     :box (:line-width 1)))))
-         (mode-line-buffer-id ((t (:foreground nil :background nil :bold nil))))
-         (mode-line-inactive ((t (:inherit mode-line
-                                           :foreground ,(third foregrounds)
-                                           :background ,background :bold nil
-                                           :box (:line-width 1)))))
-         (minibuffer-prompt ((t (:foreground ,blue))))
-         (region ((t (:background ,(second contrast-backgrounds)))))
-         (secondary-selection ((t (:foreground ,(second foregrounds)))))
+           ;; Emacs interface
+           (fringe ((t (:background ,alt-background))))
+           (border ((t (:background ,alt-background))))
+           (border-glyph ((t (nil))))
+           (highlight ((t (:background ,(second contrast-backgrounds)))))
+           (gui-element ((t (:background ,alt-background :foreground ,foreground))))
+           (mode-line ((t (:foreground ,(fourth foregrounds) :background ,alt-background :bold t
+                                       :box (:line-width 1)))))
+           (mode-line-buffer-id ((t (:foreground nil :background nil :bold nil))))
+           (mode-line-inactive ((t (:inherit mode-line
+                                             :foreground ,(third foregrounds)
+                                             :background ,background :bold nil
+                                             :box (:line-width 1)))))
+           (minibuffer-prompt ((t (:foreground ,blue))))
+           (region ((t (:background ,(second contrast-backgrounds)))))
+           (secondary-selection ((t (:foreground ,(second foregrounds)))))
 
-         ;; Parenthesis matching
-         (show-paren-match ((t (:background nil :foreground nil :inverse-video t))))
-         (show-paren-mismatch ((t (:background ,magenta :foreground ,background))))
+           ;; Parenthesis matching
+           (show-paren-match ((t (:background nil :foreground nil :inverse-video t))))
+           (show-paren-mismatch ((t (:background ,magenta :foreground ,background))))
 
-         (slime-highlight-edits-face ((t (:foreground ,(first foregrounds)))))
-         (magit-item-highlight ((t (:inherit highlight :background nil))))
+           (slime-highlight-edits-face ((t (:foreground ,(first foregrounds)))))
+           (magit-item-highlight ((t (:inherit highlight :background nil))))
 
-         (link ((t (:foreground nil :underline t))))
-         (widget-button ((t (:underline t))))
-         (org-link ((t (:foreground ,blue :underline t))))
-         (org-date ((t (:foreground ,blue :underline t))))
-         (org-done ((t (:foreground ,green))))
-         (org-todo ((t (:foreground ,red))))
-         (org-special-keyword ((t (:foreground ,orange))))
-         (org-level-1 ((t (:foreground ,blue))))
-         (org-level-2 ((t (:foreground ,foreground))))
-         (org-level-3 ((t (:foreground ,violet))))
-         (org-column ((t (:background ,alt-background))))
-         (org-warning ((t (:bold t :foreground ,red))))
-         (org-scheduled-previously ((t (:foreground ,orange))))
+           (link ((t (:foreground nil :underline t))))
+           (widget-button ((t (:underline t))))
+           (org-link ((t (:foreground ,blue :underline t))))
+           (org-date ((t (:foreground ,blue :underline t))))
+           (org-done ((t (:foreground ,green))))
+           (org-todo ((t (:foreground ,red))))
+           (org-special-keyword ((t (:foreground ,orange))))
+           (org-level-1 ((t (:foreground ,blue))))
+           (org-level-2 ((t (:foreground ,foreground))))
+           (org-level-3 ((t (:foreground ,violet))))
+           (org-column ((t (:background ,alt-background))))
+           (org-warning ((t (:bold t :foreground ,red))))
+           (org-scheduled-previously ((t (:foreground ,orange))))
 
-         (hl-sexp-face ((t (:background ,alt-background))))
-         (highlight-80+ ((t (:background ,alt-background))))
-         )))))
+           (hl-sexp-face ((t (:background ,alt-background))))
+           (highlight-80+ ((t (:background ,alt-background))))
+           ))))))
 
 ;;;###autoload
 (defun color-theme-sanityinc-solarized-dark ()
