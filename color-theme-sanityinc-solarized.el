eabe2ac611ac11232a2f829ba21b7d5b2c1f46a8
Steve Purcell <steve@sanityinc.com>
2012-12-06 08:33:16 +0000

    Add which-func face (see purcell/color-theme-sanityinc-tomorrow#5)

--- color-theme-sanityinc-solarized.el
+++ color-theme-sanityinc-solarized.el
@@ -178,6 +178,9 @@ names to which it refers are bound."
      (ido-indicator ((,class (:foreground ,red :background ,background))))
      (ido-virtual ((,class (:foreground ,faintest))))
 
+     ;; which-function
+     (which-func ((,class (:foreground ,blue :background nil :weight bold))))
+
      ;; Emacs interface
      (cursor ((,class (:background ,magenta))))
      (fringe ((,class (:background ,alt-background))))
