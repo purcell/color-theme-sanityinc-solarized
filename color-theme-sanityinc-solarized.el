3c52196742c02677308a2ec2be865c74790ea55d
Steve Purcell <steve@sanityinc.com>
2013-11-13 16:13:49 +0000

    Better docstrings

--- color-theme-sanityinc-solarized.el
+++ color-theme-sanityinc-solarized.el
@@ -26,14 +26,21 @@
 ;; Here are two slightly subdued color themes that are easy on the eyes
 ;; and cover a reasonably complete set of faces.
 
-;; In Emacs versions without built-in theme support, ie. < 24,
-;; color-theme.el is required.
+;; The themes are designed for use with Emacs' built-in theme support
+;; in Emacs 24. However, they also work with older Emacs versions, in
+;; which case color-theme.el is required.
 
-;; Use:
+;; Usage:
 
-;;   M-x color-theme-solarized-light
+;; If your Emacs has the `load-theme' command, you can use it to
+;; activate one of these themes programatically, or use
+;; `customize-themes' to select a theme interactively.
 
-;;   M-x color-theme-solarized-dark
+;; Alternatively, or in older Emacs versions, use one of the provided
+;; wrapper commands to activate a theme:
+
+;;     M-x color-theme-solarized-light
+;;     M-x color-theme-solarized-dark
 ;;
 ;;; Credit:
 
